75beaf5d08448723b10184f7afe82e988e9198c8
tasnmaysg <tanmaysg@gmail.com>
2022-01-17 22:54:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import CardList from './CardList';
+import SearchBox from './Searchbox';
+import React from 'react';
 
-function App() {
+class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state={
+      robots: [],
+      searchField : '',
+    }
+  }
+
+  onInputChange=async(val) => {
+    await this.setState({searchField : val})
+    console.log(this.state.searchField);
+  }
+
+componentDidMount = () => {
+  // console.log("Component did mount is running...")
+  // await this.setState({robots : robots})
+  console.log("Component Did Mount is running");
+  fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {
+        // console.log(users);
+        this.setState({robots : users})
+      })
+}
+
+render(){
+  console.log('Render is running...');
+
+  const {robots, searchField} = this.state;
+
+  const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase())
+  })
+
+  if (!robots.length) {    // robots.length === 0  means we want to run
+    return <h1>Loading....</h1>
+  }
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tc">
+      <h1>RoboProject</h1>
+      <SearchBox onInputChange={this.onInputChange}/> 
+      
+      <CardList robots={filteredRobots}/>
     </div>
   );
 }
-
+}
 export default App;
